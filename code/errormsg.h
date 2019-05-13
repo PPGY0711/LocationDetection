895a260383eeed50a11cfcbe735c8a1e077d35e2
PPGY0711 <49770080+PPGY0711@users.noreply.github.com>
2019-05-14 01:31:19 +0800

    Create errormsg.h

---
+++ code/errormsg.h
@@ -0,0 +1,14 @@
+/**
+** statement of error report function
+** @author:3160104633@zju.edu.cn
+** @date:2019/5/13
+**/
+
+#ifndef _ERRORMSG_H_
+#define _ERRORMSG_H_
+#include <stdarg.h>
+#include <stdio.h>
+
+void errorReport(char* errormsg, ...);
+
+#endif // _ERRORMSG_H
